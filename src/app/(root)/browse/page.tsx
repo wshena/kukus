8e3eb50ecee968c04ead15dc63f15b85b3da5f77
poppywydy <adeliasekar7@gmail.com>
2025-03-30 10:34:57 +0700

    dev: add browse page.

---
+++ src/app/(root)/browse/page.tsx
@@ -0,0 +1,12 @@
+import MainWrapper from '@/components/wrapper/MainWrapper'
+import React from 'react'
+
+const page = () => {
+  return (
+    <MainWrapper>
+      <div>page</div>
+    </MainWrapper>
+  )
+}
+
+export default page
